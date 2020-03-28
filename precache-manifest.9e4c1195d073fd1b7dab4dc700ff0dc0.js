159a1cc382912e6be9ac3adbd24792fb1e2d5881
manelturki <menelturki@gmail.com>
2020-03-28 11:43:53 -0700

    Updates

---
+++ precache-manifest.9e4c1195d073fd1b7dab4dc700ff0dc0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227b9aad41ddea69f4bb7de91d477d1b",
+    "url": "/myproject/index.html"
+  },
+  {
+    "revision": "b3fdd02a11f9f6226546",
+    "url": "/myproject/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "4f2ab0df42e649b4c379",
+    "url": "/myproject/static/css/main.f52334d7.chunk.css"
+  },
+  {
+    "revision": "b3fdd02a11f9f6226546",
+    "url": "/myproject/static/js/2.3639f34e.chunk.js"
+  },
+  {
+    "revision": "5adaff15f30f8fb7316ca97fba8ef8b8",
+    "url": "/myproject/static/js/2.3639f34e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f2ab0df42e649b4c379",
+    "url": "/myproject/static/js/main.3a7139b7.chunk.js"
+  },
+  {
+    "revision": "c3696022a1a3e28bb345",
+    "url": "/myproject/static/js/runtime-main.c3d11afb.js"
+  },
+  {
+    "revision": "f2e7fd434df64c8f77606e54e6eeb343",
+    "url": "/myproject/static/media/aa.f2e7fd43.jpg"
+  },
+  {
+    "revision": "aa50bf367b1595e7bf8bac56dfd68145",
+    "url": "/myproject/static/media/qq.aa50bf36.gif"
+  }
+]);
